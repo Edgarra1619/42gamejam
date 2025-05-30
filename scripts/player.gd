c18f0bfdc24f37c1744e52bd1300e635003029b8
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-05-30 22:15:26 +0100

    Spawn potions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,19 +1,19 @@
 extends CharacterBody2D
 
 @export var movement_speed: int = 300
-
+@export var potion: PackedScene
 
 func _ready() -> void:
 	pass
 
-
 func _process(delta: float) -> void:
-	pass
-
+	if Input.is_action_just_pressed("throw_potion"):
+		var new_potion = potion.instantiate()
+		new_potion.position = get_global_mouse_position()
+		get_tree().current_scene.add_child(new_potion)
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
-	
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
 	direction = direction.normalized()
