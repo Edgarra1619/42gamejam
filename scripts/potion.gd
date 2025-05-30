80033be3d3cce1f96838b93fc28140039662d9da
Edgar Amorim <edgribei@student.42porto.com>
2025-05-30 23:52:25 +0100

    you can now throw potions

--- scripts/potion.gd
+++ scripts/potion.gd
@@ -2,31 +2,32 @@ class_name Potion
 extends Area2D
 
 const	ROTATION_SPEED = 8
-const	MAX_SPEED = 10
+const	MAX_SPEED = 300
 
-@export var splash : PackedScene
-
-@onready var _sprite : Sprite2D = $"Sprite2D"
+static var potion : PackedScene = preload("res://scenes/potion.tscn")
+static var splash : PackedScene = preload("res://scenes/potion.tscn")
 
 var target_position : Vector2
 
-func	_init(pos : Vector2) -> void:
-	target_position = pos
+static func new_potion(pos : Vector2) -> Potion:
+	var newpot : Potion = potion.instantiate()
+	newpot.target_position = pos
+	return newpot
 
-func	_process(delta: float) -> void:
-	_sprite.rotate(delta * ROTATION_SPEED)
+func _process(delta: float) -> void:
+	rotate(delta * ROTATION_SPEED)
 
-func	_physics_process(delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	global_position = global_position.move_toward(target_position, delta * MAX_SPEED)
 	if (global_position.is_equal_approx(target_position)):
 		_explode();
 
-func	_explode() -> void:
+func _explode() -> void:
 	var splash_node := splash.instantiate()
 	splash_node.global_position = global_position
 	get_tree().current_scene.add_child(splash_node)
 	queue_free()
 
-func	_on_body_entered(_body:Node2D) -> void:
+func _on_body_entered(_body:Node2D) -> void:
 	_explode()
 
