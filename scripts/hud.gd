ff87efb2aa477b697e57e711a5551555df6c3a51
Edgarra1619 <edgarr.amorim@gmail.com>
2025-06-01 00:09:54 +0100

    hud now uses Player.player
    instead of having its own reference

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,13 +1,11 @@
 class_name Hud
 extends Control
 
-@export var player: Player
-
 func update_current_potion() -> void:
-	$CurrentPotion.text = str("Current Potion: ", player.current_potion)
+	$CurrentPotion.text = str("Current Potion: ", Player.player.current_potion)
 
 func update_potion_counts() -> void:
-	$RedPotionsCount.text = str("Red Potions: ", player.brewed_potions[0])
-	$PurplePotionsCount.text = str("Purple Potions: ", player.brewed_potions[1])
-	$GreenPotionsCount.text = str("Green Potions: ", player.brewed_potions[2])
-	$GoldPotionsCount.text = str("Gold Potions: ", player.brewed_potions[3])
+	$RedPotionsCount.text = str("Red Potions: ", Player.player.brewed_potions[0])
+	$PurplePotionsCount.text = str("Purple Potions: ", Player.player.brewed_potions[1])
+	$GreenPotionsCount.text = str("Green Potions: ", Player.player.brewed_potions[2])
+	$GoldPotionsCount.text = str("Gold Potions: ", Player.player.brewed_potions[3])
