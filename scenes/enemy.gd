43c467952dcef6a00c88856e799e334b70393ba9
mord-tirith <thiagohfreitasn@gmail.com>
2025-05-31 13:08:57 +0100

    Started enemy global logic

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,17 +1,27 @@
+class_name Enemy
 extends CharacterBody2D
 
 @export var movement_speed: int = 400
-var health: int = 3
+@export var health: int = 3
+
+var goldified: bool = false
+var gold_timer: float = 0.0
 
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	if goldified:
+		gold_timer -= delta;
+		if gold_timer <= 0:
+			break_free()
 	if (health <= 0):
 		_die()
 
 func _physics_process(delta: float) -> void:
-	if (Player.player):
+	if (goldified):
+		velocity = Vector2.ZERO
+	elif (Player.player):
 		var direction = global_position.move_toward(Player.player.global_position, movement_speed * delta) - global_position
 		velocity = direction / delta
 	move_and_slide()
@@ -23,3 +33,15 @@ func _die() -> void:
 
 func receive_damage(damage: int) -> void:
 	health -= damage
+
+func turn_gold(duration: float = 2.0) -> void:
+	if (duration <= 1.0):
+		return
+	goldified = true
+	gold_timer = duration
+	velocity = Vector2.ZERO
+	$Sprite2D.modulate = Color.GOLD
+	
+func break_free():
+	goldified = false
+	$Sprite2D.modulate = Color.WHITE
