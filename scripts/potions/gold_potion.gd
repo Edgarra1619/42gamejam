ae20090aaa802344102b3698b25a20ffbdfaa8ba
VicLourenco <134103080+VicLourenco40@users.noreply.github.com>
2025-06-01 19:21:56 +0100

    Update potion texture

--- scripts/potions/gold_potion.gd
+++ scripts/potions/gold_potion.gd
@@ -6,7 +6,6 @@ static func new_potion(pos : Vector2) -> GoldPotion:
 	var newpot : GoldPotion = potion.instantiate()
 	newpot.target_position = pos
 	newpot.impact_damage = 0
-	newpot.modulate = Color.GOLD
 	return newpot
 
 func _explode():
--- scripts/potions/purple_potion.gd
+++ scripts/potions/purple_potion.gd
@@ -6,7 +6,6 @@ static func new_potion(pos: Vector2) -> PurplePotion:
 	var newpot : PurplePotion = potion.instantiate()
 	newpot.target_position = pos
 	newpot.impact_damage = 0
-	newpot.modulate = Color.PURPLE
 	return newpot
 
 func _explode():
